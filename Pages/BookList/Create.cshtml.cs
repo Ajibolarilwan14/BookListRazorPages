a0199f5079ba9e1a7d37b8b9bd52fd6fbbcf5e8a
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-07-05 14:34:11 -0700

    Create book functionality added

--- Pages/BookList/Create.cshtml.cs
+++ Pages/BookList/Create.cshtml.cs
@@ -16,9 +16,39 @@ namespace BookListRazorPages.Pages.BookList
     {
             _db = db;
     }
+        //method #2 to post data into the db is to bind it with the model automatically
+        [BindProperty]
         public Book Book { get; set; }
         public void OnGet()
         {
+        }
+
+        //method #1 to post data into the db
+        //public async Task<IActionResult> OnPost(Book BookObj)
+        //{
+        //    //do something here
+        //}
+
+        //using method #2
+        public async Task<IActionResult> OnPost()
+        {
+            if (!(ModelState.IsValid))
+            {
+                //return back to the current page with error message
+                return Page();
+            }
+            else
+            {
+                //collect data and add to queue
+                await _db.Book.AddAsync(Book);
+
+                //save data into the database
+                await _db.SaveChangesAsync();
+
+                //redirect back to the index page
+                return RedirectToPage("Index");
+
+            }
         }
     }
 }
