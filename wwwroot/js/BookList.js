eaca7a196bde8736596480a58a54d95ede71f6a8
Adebayo Rilwan Ajibola <ajibolarilwan14@gmail.com>
2021-07-06 15:55:45 -0700

    Added DataTables, Ajax, Controller & implement GET Api call

---
+++ wwwroot/js/BookList.js
@@ -0,0 +1,41 @@
+﻿let datatable;
+
+const loadDataTable = () => {
+    //datatable = document.getElementById("DT_load");
+
+    datatable = $('#DT_load').DataTable({
+        "ajax": {
+            "url": "api/book",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "30%" },
+            { "data": "author", "width": "30%" },
+            { "data": "isbn", "width": "30%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `
+                        <div class="text-center">
+                            <a href="/BookList/Edit?id=${data}" class="btn btn-success btn-sm text-white" style="cursor: pointer; width:70px">Edit</a>
+                            &nbsp;
+                            <a class="btn btn-success btn-sm text-white" style="cursor: pointer; width:70px">Delete</a>
+                        </div>
+                    `;
+                }, "width": "100%"
+            }
+        ],
+        "language": {
+            "emptyTable": "sorry, no data at the moments!"
+        },
+        "width": "100%"
+    });
+}
+
+
+
+
+window.addEventListener('DOMContentLoaded', e => {
+    loadDataTable();
+});
